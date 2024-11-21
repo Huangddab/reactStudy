ff06d303311da2db56303a8a50b6cca85da975ec
huang-dandd <huangddab@163.com>
2024-11-21 08:27:31 +0800

    无学习

--- React 基础 - day04+day05/03-code/react-bill/src/index.js
+++ React 基础 - day04+day05/03-code/react-bill/src/index.js
@@ -9,11 +9,7 @@ console.log("====================================");
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // <React.StrictMode>
+  // 严格模式
   <App />
   // </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
