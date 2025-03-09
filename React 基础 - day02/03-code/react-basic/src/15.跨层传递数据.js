e37d785283364a8a914b3e78dc47e3f78779b23a
huang-dandd <huangddab@163.com>
2025-03-09 13:03:18 +0800

    review day1 and day2

--- React 基础 - day02/03-code/react-basic/src/15.跨层传递数据.js
+++ React 基础 - day02/03-code/react-basic/src/15.跨层传递数据.js
@@ -1,43 +1,39 @@
 // App -> A -> B
 
-import { createContext, useContext } from "react"
+import { createContext, useContext } from "react";
 
 // 1. createContext方法创建一个上下文对象
 
-const MsgContext = createContext()
+const MsgContext = createContext();
 
 // 2. 在顶层组件 通过Provider组件提供数据
 
 // 3. 在底层组件 通过useContext钩子函数使用数据
 
-function A () {
+function A() {
   return (
-    <div>
+    <div style={{ border: "1px solid red" }}>
       this is A component
       <B />
     </div>
-  )
+  );
 }
 
-function B () {
-  const msg = useContext(MsgContext)
-  return (
-    <div>
-      this is B compnent,{msg}
-    </div>
-  )
+function B() {
+  const msg = useContext(MsgContext);
+  return <div style={{ border: "2px solid blue" }}>this is B compnent,{msg}</div>;
 }
 
-function App () {
-  const msg = 'this is app msg'
+function App() {
+  const msg = "this is app msg";
   return (
-    <div>
+    <div style={{ border: "3px solid green" }}>
       <MsgContext.Provider value={msg}>
         this is App
         <A />
       </MsgContext.Provider>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
